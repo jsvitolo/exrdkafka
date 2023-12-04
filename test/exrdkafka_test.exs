e563832b5edfb5ba3f5c3faa51053252e6d5d84c
Jones Vitolo <jsvitolo@gmail.com>
2023-12-04 10:21:07 -0300

    chore: add more tests

--- test/exrdkafka_test.exs
+++ test/exrdkafka_test.exs
@@ -10,6 +10,27 @@ defmodule ExrdkafkaTest do
     end
   end
 
+  defmodule Consumer do
+    @moduledoc false
+
+    @behaviour Exrdkafka.ConsumerCallback
+
+    @impl Exrdkafka.ConsumerCallback
+    def init(_topic, _partition, _offset, _args) do
+      {:ok, %{}}
+    end
+
+    @impl Exrdkafka.ConsumerCallback
+    def handle_message(_message, %{}) do
+      {:ok, %{}}
+    end
+
+    @impl Exrdkafka.ConsumerCallback
+    def stats_callback(_client, _stats) do
+      :ok
+    end
+  end
+
   setup do
     :ok = Exrdkafka.start()
 
@@ -25,11 +46,28 @@ defmodule ExrdkafkaTest do
     end)
   end
 
-  test "create producer" do
+  test "create_producer/2 creates a producer" do
     assert Exrdkafka.create_producer(:test_producer, socket_keepalive_enable: true)
 
     assert Exrdkafka.create_producer(:other_test_producer, [])
 
     assert {:error, :err_already_existing_client} = Exrdkafka.create_producer(:test_producer, [])
+  end
+
+  test "create_consumer_group/5 creates a consumer group" do
+    client_id = :test_consumer
+    group_id = "test_group"
+    topics = [{"test_topic", [callback_module: ExrdkafkaTest.Consumer]}]
+    client_config = []
+    default_topics_config = []
+
+    assert :ok =
+             Exrdkafka.create_consumer_group(
+               client_id,
+               group_id,
+               topics,
+               client_config,
+               default_topics_config
+             )
   end
 end
