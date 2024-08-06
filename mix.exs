cb29aa1948977a6fdbc0b377ec8a9446824b5593
Jones Vitolo <jsvitolo@gmail.com>
2024-08-05 23:15:25 -0300

    chore: test withou aliases

--- mix.exs
+++ mix.exs
@@ -8,11 +8,11 @@ defmodule Exrdkafka.MixProject do
       elixir: "~> 1.14",
       compilers: [:elixir_make | Mix.compilers()],
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
+      # aliases: aliases(),
       deps: deps(),
       make_targets: ["all"],
       make_clean: ["clean"],
-      erlc_options: erlc_options()
+      # erlc_options: erlc_options()
     ]
   end
 
@@ -33,29 +33,29 @@ defmodule Exrdkafka.MixProject do
     ]
   end
 
-  defp aliases do
-    [
-      compile: ["compile", &compile_nif/1],
-      clean: ["clean", &clean_nif/1]
-    ]
-  end
+  # defp aliases do
+  #   [
+  #     compile: ["compile", &compile_nif/1],
+  #     clean: ["clean", &clean_nif/1]
+  #   ]
+  # end
 
-  defp erlc_options do
-    [
-      warnings_as_errors: true,
-      warn_export_all: true
-    ]
-  end
+  # defp erlc_options do
+  #   [
+  #     warnings_as_errors: true,
+  #     warn_export_all: true
+  #   ]
+  # end
 
-  defp compile_nif(_) do
-    if match?({:unix, _}, :os.type()) do
-      {_result, _} = System.cmd("make", ["compile_nif"], stderr_to_stdout: true)
-    end
-  end
+  # defp compile_nif(_) do
+  #   if match?({:unix, _}, :os.type()) do
+  #     {_result, _} = System.cmd("make", ["compile_nif"], stderr_to_stdout: true)
+  #   end
+  # end
 
-  defp clean_nif(_) do
-    if match?({:unix, _}, :os.type()) do
-      {_result, _} = System.cmd("make", ["clean_nif"], stderr_to_stdout: true)
-    end
-  end
+  # defp clean_nif(_) do
+  #   if match?({:unix, _}, :os.type()) do
+  #     {_result, _} = System.cmd("make", ["clean_nif"], stderr_to_stdout: true)
+  #   end
+  # end
 end
