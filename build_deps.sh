16064594006a8d3384e33d119ac30e20e3aaf82d
Jones Vitolo <jsvitolo@gmail.com>
2024-08-06 21:57:32 -0300

    chore: wip compile

--- build_deps.sh
+++ build_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ROOT=$(pwd)
-DEPS_LOCATION=_build/deps
+DEPS_LOCATION=_build/deps/exrdkafka
 OS=$(uname -s)
 KERNEL=$(echo $(lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 | awk '{print $1;}') | awk '{print $1;}')
 CPUS=`getconf _NPROCESSORS_ONLN 2>/dev/null || sysctl -n hw.ncpu`
